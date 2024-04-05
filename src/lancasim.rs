5afd42e9517ad85f291d453bcec7a41c8b155f9a
Mathuiss <mathijsgeerlings@gmail.com>
2024-04-05 12:40:43 +0200

    1.0 beta release

--- src/lancasim.rs
+++ src/lancasim.rs
@@ -6,10 +6,12 @@ use crate::{
     models::{Battlefield, SimulationResult, TurnResult},
 };
 
+/// Runs all simulations as specified in the configuration file.
 pub fn run_all(
     startup_config: &StartupConfig,
     tx: Sender<SimulationResult>,
 ) -> Result<(), LancasterError> {
+    // Calculate amount of simulations to run
     let blue_min = startup_config.blue_team().min_ac();
     let blue_max = startup_config.blue_team().max_ac();
     let blue_inc = startup_config.blue_team().increment();
@@ -22,10 +24,12 @@ pub fn run_all(
     let red_incs = ((red_max - red_min) / red_inc).abs() as i64;
     let total_simulations = blue_incs * red_incs;
 
+    // Show amount of simulations to run
     println!("Running {} simulations...", total_simulations);
 
     let mut blue_ac = blue_min;
 
+    // Run simulations
     while blue_ac <= blue_max {
         let mut red_ac = red_min;
         while red_ac <= red_max {
@@ -46,6 +50,14 @@ pub fn run_all(
     Ok(())
 }
 
+/// Tun one simulation based on:
+/// - The number of blue units
+/// - The number of red units
+/// - The blue attrition coefficient
+/// - The red attrition coefficient
+///
+/// The attrition coefficient if the effectiveness of the team.
+/// A high blue attrition coefficient means lots of damage to red.
 fn run_simulation(
     tx: Sender<SimulationResult>,
     blue_units: i64,
@@ -53,9 +65,12 @@ fn run_simulation(
     blue_ac: f32,
     red_ac: f32,
 ) -> Result<(), SendError<SimulationResult>> {
+    // Set up initial simulation information
     let mut turn_nr = 1;
     let mut battlefield = Battlefield::new(blue_units, red_units, blue_ac, red_ac);
     let mut turn_results: Vec<TurnResult> = Vec::new();
+
+    // Create information for turn 0, when the simulation has not started running yet
     turn_results.push(TurnResult::new(
         0,
         battlefield.blue_units(),
@@ -64,6 +79,7 @@ fn run_simulation(
         0,
     ));
 
+    // Simulate engagements until one team is out of units
     while battlefield.blue_units() > 0 && battlefield.red_units() > 0 {
         let (blue_casualties, red_casualties) = battlefield.simulate_turn();
         let turn_result = TurnResult::new(
@@ -79,6 +95,7 @@ fn run_simulation(
         turn_nr += 1;
     }
 
+    // Create simulation results
     let simulation_result = SimulationResult::new(
         blue_ac,
         red_ac,
@@ -87,6 +104,8 @@ fn run_simulation(
         battlefield.red_units(),
         turn_results,
     );
+
+    // Send results to file writer
     tx.send(simulation_result)?;
 
     Ok(())
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
+//! # Lancasim
+//!
+//! ## Simulate battle outcomes with Lancasim
+//!
+//! Lancasim is a high-speed simulation environment. It can run many simulations of a battle using [Lanchester's square law](https://en.wikipedia.org/wiki/Lanchester%27s_laws). Results are written concurrently so that file operations do not influence the speed of the simulations. Use lancasim to find the attrition coefficient each team needs to beat the other, given the number of units both teams have.
+//!
+//! ### Prerequisites
+//!
+//! - [Cargo](https://www.rust-lang.org/learn/get-started) if you want to compile from source.
+//! - A CSV analysis tool like [SandDance for VSCode](https://marketplace.visualstudio.com/items?itemName=msrvida.vscode-sanddance)
+//!
+//! ### Usage
+//!
+//! Lancasim is able to run many simulations of a battle, using the [Lanchester's square law](https://en.wikipedia.org/wiki/Lanchester%27s_laws). This is done based on a configuration file like this:
+//!
+//! ```ini
+//! [Blue]
+//! ; integer: Start with this amount of units
+//! units       = 100
+//! ; f32 between 0 and 1: Minimum attrition coefficient for this team
+//! min_ac      = 0.1
+//! ; f32 between 0 and 1: Maximum attrition coefficient for this team
+//! max_ac      = 0.2
+//! ; f32 between 0 and 1: Increment for each simulation
+//! increment   = 0.01
+//!
+//! [Red]
+//! ; integer: Start with this amount of units
+//! units       = 100
+//! ; f32 between 0 and 1: Minimum attrition coefficient for this team
+//! min_ac      = 0.01
+//! ; f32 between 0 and 1: Maximum attrition coefficient for this team
+//! max_ac      = 0.05
+//! ; f32 between 0 and 1: Increment for each simulation
+//! increment   = 0.001
+//! ```
+//!
+//! The simulation environment will run a simulation for all points on a `blue` by `red` matrix, where the attrition coefficients for each team increases incrementally. It will then concurrently write the results to a file in the `results` directory.
+//!
+//! **Running simulations**
+//!
+//! Starting lancasim with the `--help` option will show the following:
+//!
+//! ```text
+//!          _       ___   _   _ _____   ___   _____ ________  ___
+//!         | |     / _ \ | \ | /  __ \ / _ \ /  ___|_   _|  \/  |
+//!         | |    / /_\ \|  \| | /  \// /_\ \\ `--.  | | | .  . |
+//!         | |    |  _  || . ` | |    |  _  | `--. \ | | | |\/| |
+//!         | |____| | | || |\  | \__/\| | | |/\__/ /_| |_| |  | |
+//!         \_____/\_| |_/\_| \_/\____/\_| |_/\____/ \___/\_|  |_/
+//!
+//!
+//!         Simulate battle results with Lancaster simulation.
+//!
+//! Usage: lancasim [OPTIONS] <file>
+//!
+//! Arguments:
+//!   <file>  Path to the .ini file. Example: ./path/to/file.ini
+//!
+//! Options:
+//!   -f, --full-results  Output full battle diagnostics for each engagement. BEWARE: This is computationally expensive.
+//!   -h, --help          Print help
+//!   -V, --version       Print version
+//! ```
+
 use crate::{error::ConfigError, writer::spawn_writer};
 use clap::{Arg, ArgAction, Command};
 use config::StartupConfig;
@@ -11,7 +76,7 @@ mod lancasim;
 mod models;
 mod writer;
 
-/// Some type of explanation of **lancasim**
+/// Main function returns `()` or `LancasterError`
 fn main() -> Result<(), LancasterError> {
     let cmd = Command::new("lancasim")
         .version("1.0")
